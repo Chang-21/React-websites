979a6c1ef3ec9985c360b3ae471088c5839b125a
chang <changkuoth80@gmail.com>
2025-01-06 14:49:14 +0300

    made some updates to the stylessheet and the component

--- prac-Dev/src/UserProfile.jsx
+++ prac-Dev/src/UserProfile.jsx
@@ -4,7 +4,7 @@ import  react from "react"
 function UserProfile() {
     return (
         <div className="UserProfile">
-            <img src="https://via.placeholder.com/100" alt="profile picture" className="UserPicture"/>
+            <img src="https://via.placeholder.com/150" alt="profile picture" className="UserPicture"/>
             <h4 className="UserName">Chang</h4>
             <p className="UserBio">i am a dedicated self-taught software engineer from Kenya originally from South Sudan
             i enjoy enjoy watching and reading 
